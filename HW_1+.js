cfff33ea6bcfd9a9241d3e911250f22462ca6e45
Aleksandr Kiselev <allexxk_86@mail.ru>
2022-05-01 14:13:12 +0400

    add hw_js 1*

--- HW_1+.js
+++ HW_1+.js
@@ -1,61 +1,75 @@
-function check_age(age) {
+/*HW_1* 
+Задания с разным количеством звездочек:)
+1*:
+Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+Пример: const checkAge = function(age) {
+Ваши преобразования
+}
+Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
-   if (age < 18 && age > -1) {
+2*:
+Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+3**:
+Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+4***:
+Преобразовать задание 3* таким образом, чтобы возраст вводился используя функцию prompt в привязанной верстке*/
+
+
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
+
+/*const check_age = function(age) {
+
+   if (age < age_2) {
       console.log("You don’t have access cause your age is ", +age + " It’s less then ")
-   }
-   else if (age >= 18 && age < 60) {
+   } else if (age >= age_2 && age < age_3) {
       console.log("Welcome  !")
-   }
-   
-   else if (age > 60) {
+   } else if (age > age_3) {
       console.log("Keep calm and look Culture channel")
-   }
-   
-   else {
-      console.log("Technical work")
-   }
-}
-check_age()
-
-
-
-function check_age_1(age_1) {
-   if (typeof age_1 == "number") {
-      if (age_1 < 18 && age_1 > -1) {
-         console.log("You don’t have access cause your age is ", +age_1 + " It’s less then ")
-      }
-      else if (age_1 >= 18 && age_1 < 60) {
-         console.log("Welcome  !")
-      }
-   
-      else if (age_1 > 60) {
-         console.log("Keep calm and look Culture channel")
-      }
-   
    } else {
       console.log("Technical work")
    }
-}
-check_age_1(12)
+}*/
 
 
 
-/*function check_age_1(age_1) {
-   if (age_1 includes) {
-         +age_1
+
+/*const check_age = function(age) {
+   if (typeof age == "number") {
       
-         if (age_1 < 18 && age_1 > -1) {
-            console.log("You don’t have access cause your age is ", +age_1 + " It’s less then ")
-         }
-         else if (age_1 >= 18 && age_1 < 60) {
-            console.log("Welcome  !")
-         }
-   
-         else if (age_1 > 60) {
-            console.log("Keep calm and look Culture channel")
-         }
-   } else {
-      console.log("Technical work")
-   }
+      if (age < age_2) {
+         console.log("You don’t have access cause your age is ", +age + " It’s less then ")
+      } else if (age >= age_2 && age < age_3) {
+         console.log("Welcome  !")
+      } else if (age > age_3) {
+         console.log("Keep calm and look Culture channel")
+      } else {
+         console.log("Technical work")
+      }
+} else {console.log("it's not a number")}
 }
-check_age_1("12")*/
+check_age("jhg")
+check_age(17)*/
+
+
+
+const check_age = function(age) {
+   age = +age
+   
+   if (!isNaN(age)) {    
+      
+         if (age < age_2) {
+            console.log("You don’t have access cause your age is ", + age + " It’s less then ")
+         } else if (age >= age_2 && age < age_3) {
+            console.log("Welcome  !")
+         } else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+         } else {
+            console.log("Technical work")
+         }
+   } else {console.log("it's not a number")}
+}
+check_age("15lk")
